3954a5d58c5be3ff3af9df30eb1112c2c25605ad
Nite <solitude.loner7@gmail.com>
2020-11-06 02:10:23 +0700

    [Update] Refactor Code to PubSub principle

--- script.js
+++ script.js
@@ -1,86 +1,144 @@
+// PubSub Principle
+let mediator = {
+    events: {
 
-function removeBoard(size) {
-    let board = document.querySelector(".board");
-    let boardRow = document.querySelectorAll(".board__row");
+    },
 
-    for (let row of Array.from(boardRow)) {
-        row.remove();
-    }
-}
+    on: function (eventName, fn) {
+        this.events[eventName] = this.events[eventName] || [];
+        this.events[eventName].push(fn);
+    },
 
-function initBoard(size, boardWidth) {
-    let board = document.querySelector(".board");
-
-    let cellWidth = boardWidth / size; 
-
-    for ( let i = 0; i < size; i++ ) {
-    
-        let row = document.createElement("div");
-        row.classList.add("board__row");
-    
-        for ( let j = 0; j < size; j++ ) {
-            let column = document.createElement("div");
-            column.classList.add("board__row__column");
-
-            column.style.width = cellWidth + "px";
-            column.style.height = cellWidth - 2 + "px";
-    
-            column.addEventListener("mouseover", (event) => {
-                if ( mode == "black") {
-                    column.style.backgroundColor = "black";
-                } else {
-                    let randomColor = Math.floor(Math.random()*16777215).toString(16);
-                    column.style.backgroundColor = "#" + randomColor;
+    off: function (eventName, fn) {
+        if ( this.events[eventName] ) {
+            for ( let i = 0; i < this.events[eventName].length; i++ ) {
+                if ( this.events[eventName][i] === fn) {
+                    this.events[eventName].splice(i, 1);
+                    break; 
                 }
+            }
+        }
+    },
+
+    emit: function(eventName, data) {
+        if (this.events[eventName]) {
+            this.events[eventName].forEach((fn) => {
+                fn(data);
             })
-    
-            row.appendChild(column);
-        } 
-    
-        board.appendChild(row);
-    }
-}
-
-function clearBoard(event) {
-    let cells = document.querySelectorAll(".board__row__column");
-
-    for ( let i = 0; i < cells.length; i++ ) {
-        cells[i].style.backgroundColor = "white";
+        }
     }
 
+};
+
+
+let board = (function() {
+
+    let boardContainer = document.querySelector(".board");
+    let boardRows;
+    let size = 16;
+    let boardWidth = 320;
+    let mode = "black";
+
+    // Init board
+    initBoard();
+
+    // Bind event
+    mediator.on("modeChange", changeMode);
+    mediator.on("clearBoard", resetBoard);
+    mediator.on("sizeChange", changeSize);
+
+    function resetBoard() {    
+        removeBoard();
+        initBoard();
+    }
+
+    function removeBoard() {
+        for ( let row of Array.from(boardRows)) {
+            row.remove();
+        }
+    }
+
+    function changeSize(sizeValue) {
+        size = sizeValue;
+    }
+
+    function changeMode(modeName) {
+        mode = modeName;
+    }
+
+    function hoverCell(event) {
+        if ( mode == "black") {
+            event.target.style.backgroundColor = "black";
+        } else {
+            let randomColor = Math.floor(Math.random()*16777215).toString(16);
+            event.target.style.backgroundColor = "#" + randomColor;
+        }
+    }
+    
+    function initBoard() {
+    
+        let cellWidth = boardWidth / size; 
+    
+        for ( let i = 0; i < size; i++ ) {
+        
+            let row = document.createElement("div");
+            row.classList.add("board__row");
+        
+            for ( let j = 0; j < size; j++ ) {
+                let column = document.createElement("div");
+                column.classList.add("board__row__column");
+    
+                column.style.width = cellWidth + "px";
+                column.style.height = cellWidth - 2 + "px";
+        
+                column.addEventListener("mouseover", hoverCell);
+        
+                row.appendChild(column);
+            } 
+        
+            boardContainer.appendChild(row);
+        }
+
+        boardRows = boardContainer.querySelectorAll(".board__row");
+    }
+})();
+
+
+let options = (function() {
+    let clearButton = document.querySelector(".buttons__clear");
+    let choice = document.querySelector(".buttons__options");
     let inputSize;
+    
+    clearButton.addEventListener('click', changeBoard)
+    
+    choice.addEventListener("change", changeMode)
 
-    do {
-        inputSize = Number(prompt("Create board with size:", "16"));
-        if ( inputSize > 100 ) {
-            alert("Board size cannot be greater than 100.");
-        }
-        if ( isNaN(inputSize) ) {
-            alert("Invalid board size.");
-        }
-    } while (isNaN(inputSize) || inputSize > 100);
+    function changeMode(event) {
+        mediator.emit("modeChange", event.target.value);
+    }
+    
+    function changeBoard(event) {
 
-    removeBoard(size);
-    initBoard(inputSize, boardWidth);
-    size = inputSize;
-}
+        do {
+            inputSize = Number(prompt("Create board with size:", "16"));
+            if ( inputSize > 100 ) {
+                alert("Board size cannot be greater than 100.");
+            }
+            if ( isNaN(inputSize) ) {
+                alert("Invalid board size.");
+            }
+        } while (isNaN(inputSize) || inputSize > 100);
 
-
-let clearButton = document.querySelector(".buttons__clear");
-let choice = document.querySelector(".buttons__options");
-let mode = "black";
-
-clearButton.addEventListener('click', clearBoard)
-
-choice.addEventListener("change", (event) => {
-    mode = choice.value;
-})
+        mediator.emit("sizeChange", inputSize);
+        mediator.emit("clearBoard");
+    }
+})();
 
 
-let size = 16;
-let boardWidth = 320;
 
-initBoard(size, boardWidth);
+
+
+
 
 
 
